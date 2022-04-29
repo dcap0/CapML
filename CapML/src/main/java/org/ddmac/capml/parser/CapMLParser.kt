5fa06b089c33b7b0dd90260c3737c1cdc31e635b
Dennis Capone <dennis.d.cap@gmail.com>
2022-04-28 19:33:49 -0600

    Trying to use DRY programming

--- CapML/src/main/java/org/ddmac/capml/parser/CapMLParser.kt
+++ CapML/src/main/java/org/ddmac/capml/parser/CapMLParser.kt
@@ -1,11 +1,18 @@
 package org.ddmac.capml.parser
 
 import android.content.Context
+import android.content.res.Resources
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import com.google.android.material.checkbox.MaterialCheckBox
+import com.google.android.material.datepicker.MaterialTextInputPicker
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.textview.MaterialTextView
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
@@ -26,7 +33,10 @@ import java.util.*
  * @constructor primes the use of the parse() method.
  */
 
-class CapMLParser(private val ctx: Context) {
+class CapMLParser(
+    private val ctx: Context,
+    private val userAttr: AttributeSet? = null
+) {
 
     //the data will go here.
     var data = JsonObject()
@@ -38,73 +48,9 @@ class CapMLParser(private val ctx: Context) {
      *
      * @return a [ScrollView] containing the elements, order maintained.
      */
-
+    @Throws(CapmlFileFormatException::class, CapmlParseException::class)
     fun parse(capmlFile: File): ScrollView {
-
-        if(capmlFile.name.substringAfterLast('.')!="capml"){
-            throw CapmlFileFormatException(
-                "File ${capmlFile.name} is not of type CapML. Please provide a .capml file"
-            )
-        }
-
-        //LinearLayout to contain elements. Basic "Style"
-        val ll = LinearLayout(ctx).apply {
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT,
-            )
-            orientation = LinearLayout.VERTICAL
-        }
-
-        //Sut up buffered reader to go through bytes.
-        val fos = FileInputStream(capmlFile)
-        val br = BufferedReader(InputStreamReader(fos))
-
-        var r = 0 //EOF is -1
-        var element = 0 //int representation of the parsed element.
-        var content: LinkedList<String> = LinkedList() //Content when generating views.
-        var key: String? = null
-
-        while (r != -1) {
-            r = br.read() //read the next byte
-            if (r == -1) break //if it somehow gets here with EOF, stop
-            when (Char(r)) { //int to char to check decorators
-                '#' -> { br.readLine(); continue } //skip to end of line on comment char
-                '~' -> { //the element identifier is the bytes of the following two element chars
-                    element = br.read() + br.read()
-                    br.readLine()
-                }
-                '+' -> { //add the string following the decorator
-                    content.add(
-                        br
-                            .readLine()
-                            .replaceFirst("+", "")
-                            .trim()
-                    )
-                }
-                '=' -> {//add the key following the decorator.
-                    key = br.readLine()
-                }
-                '-' -> {//End of Element decorator. Adds all views to the linear layout.
-                    ll.addView(createElement(element, content,key))
-                    content = LinkedList()
-                    br.read()
-                }
-                else -> continue //move along. move along.
-            }
-        }
-        br.close()
-        fos.close()
-
-        //Wrap the linear layout in a scroll view. Accounts for many elements. More "Style"
-        return ScrollView(ctx)
-            .apply {
-                layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-                addView(ll)
-            }
+        return parse(capmlFile.inputStream())
     }
 
 
@@ -221,6 +167,43 @@ class CapMLParser(private val ctx: Context) {
         }
     }
 
+    /**
+     * Takes [element] value to determine the element type.
+     * Depending on the [element], the [content] may be one or many.
+     *
+     * @return the [View] subclass denoted by the [element]
+     */
+
+    private fun createElement(
+        element: Int,
+        content: LinkedList<String>,
+        key: String?,
+        attr: AttributeSet
+    ): View {
+        return when (element) {
+            133 -> {
+                if(key==null) throw CapmlParseException("CB element must have key decorator (=) and value")
+                if(!data.has(key)){ data.addProperty(key,"") }
+                createCheckBox(content.first,key,attr)
+            }
+            170 -> {
+                createTextView(content.first,attr)
+            }
+            153 -> {
+                if(key == null) throw CapmlParseException("Element ET must have key decorator (=) and value")
+                if(!data.has(key))data.addProperty(key,"")
+                createEditText(content.first,key,attr)
+            }
+            163 -> {
+                if(key==null) throw CapmlParseException("Element SP must have key decorator (=) and value")
+                if(!data.has(key)) data.addProperty(key,"")
+                createSpinner(content,key,attr)
+            }
+            else -> {
+                View(ctx)
+            }
+        }
+    }
 
 
     /**
@@ -235,6 +218,22 @@ class CapMLParser(private val ctx: Context) {
             setOnCheckedChangeListener { _, isChecked -> data.addProperty(key,isChecked) }
             isChecked = data.get(key).asBoolean
         }
+
+
+    /**
+     * Makes the [CheckBox] text content be [content].
+     * If the parser object's data has the key, it will set it checked based on it's value.
+     * Applies user provided [AttributeSet]
+     *
+     * @return [CheckBox]
+     */
+    private fun createCheckBox(content: String, key: String,attr: AttributeSet) =
+        MaterialCheckBox(ctx,attr).apply {
+            text = content
+            setOnCheckedChangeListener { _, isChecked -> data.addProperty(key,isChecked) }
+            isChecked = data.get(key).asBoolean
+        }
+
 
     /**
      * Makes the [EditText] hint content be [content]
@@ -252,6 +251,26 @@ class CapMLParser(private val ctx: Context) {
         }
 
     /**
+     * Makes the [EditText] hint content be [content]
+     * If the parser's data object has a value for the key provided,
+     * The [EditText] will be populated with that value.
+     * Applies user provided [AttributeSet]
+     *
+     * @return [EditText]
+     */
+
+    private fun createEditText(content: String, key: String, attr: AttributeSet) =
+        TextInputLayout(ctx,attr).apply {
+            this.addView(
+                TextInputEditText(ctx,attr).apply {
+                    hint = content
+                    setText((data.get(key)?:"").toString())
+                    addTextChangedListener(ETWatcher((key)))
+                }
+            )
+        }
+
+    /**
      * Makes the [TextView] text content be [content]
      *
      * @return [TextView]
@@ -259,6 +278,16 @@ class CapMLParser(private val ctx: Context) {
 
     private fun createTextView(content: String) =
         TextView(ctx).apply { text = content }
+
+    /**
+     * Makes the [TextView] text content be [content]
+     * Applies user provided [AttributeSet]
+     *
+     * @return [TextView]
+     */
+
+    private fun createTextView(content: String, attr: AttributeSet) =
+        MaterialTextView(ctx,attr).apply { text = content }
 
     /**
      * Creates a [Spinner]
@@ -279,6 +308,33 @@ class CapMLParser(private val ctx: Context) {
             adapter = ArrayAdapter(ctx, R.layout.spinner_content,content)
             onItemSelectedListener = SPListener(key)
             this.setSelection(index)
+        }
+    }
+
+    /**
+     * Creates a [Spinner]
+     * Creates an [ArrayAdapter] from the [content], with a space pushed to the front
+     * Applies the [ArrayAdapter] to the [Spinner]
+     * If the parse object's data object has a value,
+     *  that value will be selected if it exists in the [Spinner]
+     *
+     * @return [Spinner]
+     */
+
+    private fun createSpinner(content: LinkedList<String>, key: String, attr: AttributeSet): TextInputLayout {
+        content.push(" ")
+        val index = if(data.has(key)){
+            content.indexOf(data.get(key).toString())
+        } else { 0 }
+
+        return TextInputLayout(ctx,attr).apply {
+            addView(
+                AutoCompleteTextView(ctx,attr).apply {
+                    setAdapter(ArrayAdapter(ctx, R.layout.spinner_content,content))
+                    onItemSelectedListener = SPListener(key)
+                    this.setSelection(index)
+                }
+            )
         }
     }
 
