bd6c35259e105c20e41e2eb239a4221e9c815e78
Dennis Capone <dennis.d.cap@gmail.com>
2022-05-29 00:55:55 -0600

    Formatting and imports.

--- CapML/src/main/java/org/ddmac/capml/parser/CapMLParser.kt
+++ CapML/src/main/java/org/ddmac/capml/parser/CapMLParser.kt
@@ -1,10 +1,8 @@
 package org.ddmac.capml.parser
 
 import android.content.Context
-import android.content.res.Resources
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
@@ -14,7 +12,10 @@ import com.google.gson.JsonParser
 import org.ddmac.capml.R
 import org.ddmac.capml.exceptions.CapmlFileFormatException
 import org.ddmac.capml.exceptions.CapmlParseException
-import java.io.*
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStream
+import java.io.InputStreamReader
 import java.util.*
 
 /**
@@ -74,7 +75,9 @@ class CapMLParser(
             r = br.read() //read the next byte
             if (r == -1) break //if it somehow gets here with EOF, stop
             when (Char(r)) { //int to char to check decorators
-                '#' -> { br.readLine(); continue } //skip to end of line on comment char
+                '#' -> {
+                    br.readLine(); continue
+                } //skip to end of line on comment char
                 '~' -> {
                     //the element identifier is the bytes of the following two element chars
                     element = (br.read() shl 8) + br.read()
@@ -89,16 +92,16 @@ class CapMLParser(
                     )
                 }
                 '=' -> {//add the key following the decorator.
-                   key = br.readLine()
+                    key = br.readLine()
                 }
                 '-' -> {//End of Element decorator. Adds view to the linear layout.
-//                    ll.addView(
-//                        createElement(
-//                            element,
-//                            content,
-//                            key
-//                        )
-//                    )
+                    ll.addView(
+                        createElement(
+                            element,
+                            content,
+                            key
+                        )
+                    )
                     content = LinkedList()
                     br.read()
                 }
@@ -135,22 +138,24 @@ class CapMLParser(
     ): View {
         return when (element.hashCode()) {
             Widgets.CB -> {
-                if(key==null) throw CapmlParseException("CB element must have key decorator (=) and value")
-                if(!data.has(key)){ data.addProperty(key,"") }
-                createCheckBox(content.first,key)
+                if (key == null) throw CapmlParseException("CB element must have key decorator (=) and value")
+                if (!data.has(key)) {
+                    data.addProperty(key, "")
+                }
+                createCheckBox(content.first, key)
             }
             Widgets.TV -> {
                 createTextView(content.first)
             }
             Widgets.ET -> {
-                if(key == null) throw CapmlParseException("Element ET must have key decorator (=) and value")
-                if(!data.has(key))data.addProperty(key,"")
-                createEditText(content.first,key)
+                if (key == null) throw CapmlParseException("Element ET must have key decorator (=) and value")
+                if (!data.has(key)) data.addProperty(key, "")
+                createEditText(content.first, key)
             }
             Widgets.SP -> {
-                if(key==null) throw CapmlParseException("Element SP must have key decorator (=) and value")
-                if(!data.has(key)) data.addProperty(key,"")
-                createSpinner(content,key)
+                if (key == null) throw CapmlParseException("Element SP must have key decorator (=) and value")
+                if (!data.has(key)) data.addProperty(key, "")
+                createSpinner(content, key)
             }
             else -> {
                 View(ctx)
@@ -168,7 +173,7 @@ class CapMLParser(
     private fun createCheckBox(content: String, key: String) =
         CheckBox(ctx).apply {
             text = content
-            setOnCheckedChangeListener { _, isChecked -> data.addProperty(key,isChecked) }
+            setOnCheckedChangeListener { _, isChecked -> data.addProperty(key, isChecked) }
             isChecked = data.get(key).asBoolean
         }
 
@@ -183,7 +188,7 @@ class CapMLParser(
     private fun createEditText(content: String, key: String) =
         EditText(ctx).apply {
             hint = content
-            setText((data.get(key)?:"").toString())
+            setText((data.get(key) ?: "").toString())
             addTextChangedListener(ETWatcher((key)))
         }
 
@@ -208,11 +213,13 @@ class CapMLParser(
 
     private fun createSpinner(content: LinkedList<String>, key: String): Spinner {
         content.push(" ")
-        val index = if(data.has(key)){
+        val index = if (data.has(key)) {
             content.indexOf(data.get(key).toString())
-        } else { 0 }
-         return Spinner(ctx).apply {
-            adapter = ArrayAdapter(ctx, R.layout.spinner_content,content)
+        } else {
+            0
+        }
+        return Spinner(ctx).apply {
+            adapter = ArrayAdapter(ctx, R.layout.spinner_content, content)
             onItemSelectedListener = SPListener(key)
             this.setSelection(index)
         }
@@ -224,7 +231,7 @@ class CapMLParser(
      * @param [c] is the user's class of choice
      * @return the [Class] populated with the user's data.
      */
-    inline fun <reified T> dataToClass(c: Class<T>): T = Gson().fromJson(data,c)
+    inline fun <reified T> dataToClass(c: Class<T>): T = Gson().fromJson(data, c)
 
     /**
      * Function that takes user data class and translates into a JsonObject.
@@ -235,32 +242,32 @@ class CapMLParser(
 
     fun <T> classToData(c: T): JsonObject = JsonParser.parseString(Gson().toJson(c)).asJsonObject
 
-    inner class ETWatcher(private val key: String): TextWatcher{
+    inner class ETWatcher(private val key: String) : TextWatcher {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            data.addProperty(key,s.toString())
+            data.addProperty(key, s.toString())
         }
 
         override fun afterTextChanged(s: Editable?) {}
 
     }
 
-    inner class SPListener(private val key: String): AdapterView.OnItemSelectedListener{
+    inner class SPListener(private val key: String) : AdapterView.OnItemSelectedListener {
         override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
             data.addProperty(key, (view as TextView).text.toString())
         }
 
         override fun onNothingSelected(parent: AdapterView<*>?) {
-            data.addProperty(key,"")
+            data.addProperty(key, "")
         }
     }
 
-    private object Widgets{
+    private object Widgets {
         val TV: Int = "TV".hashCode()
         val ET: Int = "ET".hashCode()
         val SP: Int = "SP".hashCode()
         val CB: Int = "CB".hashCode()
     }
-    
+
 }
